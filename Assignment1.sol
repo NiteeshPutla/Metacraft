1f294192c44ddd3bf0255f9a042327b4700179ff
Niteesh Putla <90021910+NiteeshPutla@users.noreply.github.com>
2022-12-17 14:56:33 +0530

    Create Assignment1.sol

---
+++ Assignment1.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0; //solidity version
+contract greatestnum{
+
+    uint a;
+    uint b;
+    uint c;
+    uint d;
+    address admin;
+
+    constructor(){
+        admin = msg.sender;
+    }
+
+    modifier onlyAdmin { 
+        require (admin == msg.sender, "only owner can access");
+        _;
+    }
+
+    function set (uint _a, uint _b, uint _c, uint _d) public onlyAdmin{
+        a = _a;
+        b = _b;
+        c = _c;
+        d = _d;
+    }
+
+    function get() public view onlyAdmin returns(uint){
+        if ((a > b) && (a > c) && (a > d)){
+        return(a);
+        }
+        else if ((b >c ) && (b > d) && (b > a)){
+            return(b);
+        }
+        else if ((c > d) && (c > a) && (c > b)){
+            return(c);
+        }
+        else {
+            return(d);
+        }
+    }
+}
