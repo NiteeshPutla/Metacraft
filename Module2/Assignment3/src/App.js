e1fb9caa465beb875e853d926f7eb59b558da1ef
Niteesh <niteeshjoyal@gmail.com>
2022-12-22 13:56:02 +0530

    Assignment3 added

---
+++ Module2/Assignment3/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import WalletCard from './WalletCard';
+
+function App() {
+
+  return (
+    <div className="App">
+    <WalletCard/>
+
+    </div>
+  );
+}
+
+export default App;
